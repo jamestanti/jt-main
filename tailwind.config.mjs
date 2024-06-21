538e5d4e1b078790d423cb337fc199c8017e53a2
James tanti <admin@cookingvirtual.com>
2024-06-21 16:33:30 -0400

    "updated website"

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./public/**/*.astro",
+	"./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+	"./node_modules/preline/preline.js",],
+	theme: {
+		extend: {},
+	},
+	darkMode: "class",
+	plugins: [require("preline/plugin")],
+}
